644085ddad523b1b68ed1a28582c9c07de8a8d99
chandana953 <chandanacd17@gmail.com>
2026-04-18 09:55:35 +0530

    updated

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,10 +1,24 @@
 import Header from "./components/Header"
 import Todoinput from "./components/Todoinput"
+import Todolist from "./components/Todolist"
+import { useState } from "react"
+import Todoitem from "./components/Todoitem"
+
 function App() {
-  return (
+  const [todos,setTodos] = useState([])
+  
+  const handleAddTodo = (newTodo) => {
+    setTodos([...todos,newTodo])
+  }
+  const handleDeleteTodo = (index) => {
+    setTodos(todos.filter((_,i)=>i !== index))
+  }
+  return(
     <>
     <Header/>
-    <Todoinput/>
+    <Todoinput addTodo={handleAddTodo}/>
+    <Todolist todos={todos} deleteTodo={handleDeleteTodo}/>
+    
     </>
   )
 }
